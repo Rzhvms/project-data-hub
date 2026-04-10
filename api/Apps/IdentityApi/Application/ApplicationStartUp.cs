3a1f76771b3c79ca94d60af69b5bbb8db906f251
Rzhvms <160605m@mail.ru>
2026-04-11 00:47:23 +0500

    Add startup files to IdentityApi

---
+++ api/Apps/IdentityApi/Application/ApplicationStartUp.cs
@@ -0,0 +1,16 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+/// <summary>
+/// Регистрация зависимостей уровня Application
+/// </summary>
+public static class ApplicationStartUp
+{
+    /// <summary>
+    /// Добавление зависимостей
+    /// </summary>
+    public static void AddApplication(this IServiceCollection services)
+    {
+    }
+}
