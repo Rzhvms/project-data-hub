534b421978177256fedd2283a7bff6548a5791a1
Rzhvms <160605m@mail.ru>
2026-04-10 23:52:59 +0500

    Add SynchronizationApi solution

---
+++ api/Apps/SyncronizationApi/Api/Startup.cs
@@ -0,0 +1,67 @@
+using Microsoft.OpenApi;
+
+namespace Api;
+
+public sealed class Startup(IWebHostEnvironment env)
+{
+    /// <summary>
+    /// Окружение приложения
+    /// </summary>
+    private IWebHostEnvironment Environment { get; } = env;
+
+    /// <summary>
+    /// Регистрация сервисов
+    /// </summary>
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        //services.AddApplication();
+        //services.AddInfrastructure();
+        
+        services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(builder =>
+            {
+                builder
+                    .SetIsOriginAllowed(_ => true)
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+            });
+        });
+
+        services.AddEndpointsApiExplorer();
+
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "SynchronizationApi",
+                Version = "v1"
+            });
+        });
+    }
+
+    /// <summary>
+    /// Конфигурация middleware пайплайна
+    /// </summary>
+    public void Configure(IApplicationBuilder app)
+    {
+        if (Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "SynchronizationApi v1");
+            });
+        }
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
