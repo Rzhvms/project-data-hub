be7f2f672061c351251bd9066fd662b570342759
Nikolai Klimov <nik.klimov09@yandex.ru>
2026-05-09 21:12:14 +0500

    frontend: подключил taiga-ui

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TuiRoot } from '@taiga-ui/core';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, TuiRoot],
     templateUrl: './app.html',
     styleUrl: './app.scss',
-    changeDetection: ChangeDetectionStrategy.OnPush
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
     protected readonly title = signal('frontend');
